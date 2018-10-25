f71ef12889aaed4b7858344555251f4f94a21da1
Wayne Lewis <waynelewis@esss.se>
2018-10-25 21:19:21 +0200

    Explicitly identify reactor to use

--- server/start_recceiver.sh
+++ server/start_recceiver.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #env PYTHONPATH=. twistd -n -r poll recceiver -f cf.conf
-env PYTHONPATH=. twistd -n recceiver -f cf.conf
+env PYTHONPATH=. twistd -n -r poll recceiver -f ess-labs-utgard-apple-cf.conf
